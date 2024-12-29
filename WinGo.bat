8bb2609049c1ebe71df5b12d64ee78f127ffb235
Yali Masslavy <166911202+TheYali1@users.noreply.github.com>
2024-12-29 12:49:32 +0200

    Add files via upload

---
+++ WinGo.bat
@@ -0,0 +1,90 @@
+@echo off
+chcp 65001 >nul
+title WinGo - The Yali
+cls
+echo [7;31mplease click F11[0m
+echo [7;31mIf you gets errors, its fine![0m
+echo.
+echo [34m██╗    ██╗██╗███╗   ██╗ ██████╗  ██████╗     [0m
+ping localhost -n 1 >nul
+echo [94m██║    ██║██║████╗  ██║██╔════╝ ██╔═══██╗    [0m
+ping localhost -n 1 >nul
+echo [36m██║ █╗ ██║██║██╔██╗ ██║██║  ███╗██║   ██║    [0m
+ping localhost -n 1 >nul
+echo [96m██║███╗██║██║██║╚██╗██║██║   ██║██║   ██║    [0m
+ping localhost -n 1 >nul
+echo [96m╚███╔███╔╝██║██║ ╚████║╚██████╔╝╚██████╔╝    [0m
+ping localhost -n 1 >nul
+echo [96m ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝     [0m
+ping localhost -n 1 >nul
+pause
+echo.
+echo Loading...
+ping localhost -n 1 >nul
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul
+echo [96mConnecting to the main server...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 1...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 2...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 3...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 4...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 5...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 6...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 7...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 8...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 9...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 10...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 11...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 12...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 13...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 14...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 15...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 16...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 17...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 18...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 19...[0m
+slmgr /skms kms8.msguides.com >nul
+echo.
+echo [96mConnecting to server 20...[0m
+slmgr /ato >nul
+echo.
+echo [42mYour PC is activate windows successfully![0m
+echo.
+pause
